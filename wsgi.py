718861b8890bec8c742da6fd8a2d0a2db237638d
Alan Sparrow <git@freeflight.org.uk>
2023-05-01 20:06:52 +0100

    Update configuration

--- wsgi.py
+++ wsgi.py
@@ -1,3 +1,9 @@
+from werkzeug.middleware.proxy_fix import ProxyFix
+
 from asselect.asselect import create_app
 
-app = create_app("CONFIG")
+# Create the application
+app = create_app("ASSELECT_CONFIG")
+
+# Fix to run behind NGINX reverse-proxy
+nginx_app = ProxyFix(app, x_for=1, x_proto=1, x_host=1, x_prefix=1)
