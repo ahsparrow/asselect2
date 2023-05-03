ef5a855c213131995e288821e80074a9547dea7f
Alan Sparrow <git@freeflight.org.uk>
2023-05-03 17:23:21 +0100

    Typo in gunicorn conf

--- gunicorn.conf.py
+++ gunicorn.conf.py
@@ -5,5 +5,5 @@ bind = "127.0.0.1:8001"
 workers = multiprocessing.cpu_count() + 1
 loglevel = "info"
 errorlog = str(Path.home() / "log" / "asselect.log")
-wsgi_app = "wsgi:create_gunicorn_app"
+wsgi_app = "wsgi:create_gunicorn_app()"
 
