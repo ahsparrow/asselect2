612af5b66c4b0a70d0cc4c5ed040504f070a7ec6
Alan Sparrow <alan@freeflight.org.uk>
2026-07-08 15:02:25 +0100

    Add NOTAMs

--- src/components/notam_tab.rs
+++ src/components/notam_tab.rs
@@ -12,10 +12,34 @@
 //
 // You should have received a copy of the GNU General Public License
 // along with this program. If not, see <https://www.gnu.org/licenses/>.
-//
-use leptos::html::{p};
+
+use leptos::html::{a, div};
 use leptos::prelude::*;
 
 pub fn notam_tab() -> impl IntoView {
-    p().child("Notam")
+    div().child((
+        div().class("subtitle") .child("Navigation Warnings"),
+        div().class("block")
+            .child("The PDFs below show a summary of the navigation warning NOTAMs relevant to cross country gliding. The PDFs are refreshed during the day at approximately ten minutes to the hour."),
+        div().class("block ml-4")
+            .child(a()
+                .attr("href", "https://navplot.asselect.uk/today_south.pdf")
+                .attr("download", "")
+                .child("Download Today (England/Wales) PDF")),
+        div().class("block ml-4")
+            .child(a()
+                .attr("href", "https://navplot.asselect.uk/today_north.pdf")
+                .attr("download", "")
+                .child("Download Today (North England/Scotland) PDF")),
+        div().class("block ml-4")
+            .child(a()
+                .attr("href", "https://navplot.asselect.uk/tomorrow_south.pdf")
+                .attr("download", "")
+                .child("Download Tomorrow (England/Wales) PDF")),
+        div().class("block ml-4")
+            .child(a()
+                .attr("href", "https://navplot.asselect.uk/tomorrow_north.pdf")
+                .attr("download", "")
+                .child("Download Tomorrow (North England/Scotland) PDF")),
+    ))
 }
